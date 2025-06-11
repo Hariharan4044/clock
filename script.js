6342930da476709dcb44a3679bed6a879e8c5983
Hariharan <146200143+Hariharan4044@users.noreply.github.com>
2025-06-11 21:12:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const updateTime = () => {
     secToDeg = (date.getSeconds() / 60) * 360,
     minToDeg = (date.getMinutes() / 60) * 360,
     hrToDeg = (date.getHours() / 12) * 360;
-  // Rotate the clock hands to the appropriate degree based on the current time
+  // Rotate the clock hands to the appropriate degree based on  current time
   secondHand.style.transform = `rotate(${secToDeg}deg)`;
   minuteHand.style.transform = `rotate(${minToDeg}deg)`;
   hourHand.style.transform = `rotate(${hrToDeg}deg)`;
